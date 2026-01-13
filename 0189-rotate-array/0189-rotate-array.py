c9f4a2cf77a3cb8987c6465947ef7cfaaaa7ec62
Ritkaar Singh <85431642+ritkaarsingh30@users.noreply.github.com>
2026-01-13 10:55:03 +0530

    Time: 1679 ms (5.02%), Space: 26.5 MB (11.83%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        while k != 0:
+            nums.insert(0, nums[len(nums) - 1])
+            nums.pop()
+            k = k-1
+        
